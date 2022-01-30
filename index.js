0c86c5b96f11ab5b8c254fb27bdc5821dedfdf6d
lydiatayo <94493453+lydiatayo@users.noreply.github.com>
2022-01-30 11:40:34 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Capstone from './components/Capstone.css';
+import Contact from './components/Contact';
+import Blog from './components/Blog';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Contact />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
